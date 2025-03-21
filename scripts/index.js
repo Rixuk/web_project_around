a7a20d0c85de8aac14b0d00d418c34ed06c735ee
Rixuk <hugomh90@hotmail.com>
2025-03-21 15:27:35 -0600

    Add cards with JavaScript and change CSS url with HTML src images

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,47 @@ const editProfile = profile.querySelector(".profile__button");
 const popup = content.querySelector(".popup");
 const popupForm = popup.querySelector(".popup__form");
 const popupClose = popup.querySelector(".popup__close");
+const elements = content.querySelector(".elements");
+
+const elementsTemplate = document.querySelector("#elements__template");
+
+const initCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+initCards.forEach(({ name, link }) => {
+  const clon = elementsTemplate.content.cloneNode(true);
+  const image = clon.querySelector(".elements__image");
+  const location = clon.querySelector(".elements__location");
+
+  image.src = link;
+  location.textContent = name;
+
+  elements.appendChild(clon);
+});
 
 function showPopup() {
   popup.classList.toggle("popup__opened");
