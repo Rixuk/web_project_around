cbcbc8d27cd11a35ad39446b4a5dd161ab90fa9e
Rixuk <hugomh90@hotmail.com>
2025-11-03 17:31:23 -0600

    little changes

--- scripts/PopupWithImage.js
+++ scripts/PopupWithImage.js
@@ -3,5 +3,13 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this._image = this._popup.querySelector("#popup-images__image");
+    this._caption = this._popup.querySelector("#popup-images__location");
+  }
+  open(){
+    this._image.src = this._link;
+    this._image.alt = this._name;
+    this._caption.textContent = this._name;
+    super.open();
   }
 }
